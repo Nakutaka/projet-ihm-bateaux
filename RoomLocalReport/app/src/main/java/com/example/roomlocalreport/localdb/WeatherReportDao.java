cb324fc466bd69f6b5ba452a9ca2452ec6bf59cd
Angele Badia <angele.badia@etu.unice.fr>
2020-04-15 11:39:41 +0200

    room database project added

---
+++ RoomLocalReport/app/src/main/java/com/example/roomlocalreport/localdb/WeatherReportDao.java
@@ -0,0 +1,50 @@
+package com.example.roomlocalreport.localdb;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Transaction;
+
+import com.example.roomlocalreport.localdb.models.Incident;
+import com.example.roomlocalreport.localdb.models.Report;
+import com.example.roomlocalreport.localdb.models.ReportWithIncidents;
+
+import java.util.List;
+
+@Dao
+public interface WeatherReportDao {
+
+   // allowing the insert of the same word multiple times by passing a 
+   // conflict resolution strategy
+
+   //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+   @Insert(onConflict = OnConflictStrategy.IGNORE)
+   void insert(Report report);
+
+   @Insert(onConflict = OnConflictStrategy.IGNORE)
+   long insert(Incident incident );
+
+   @Insert(onConflict = OnConflictStrategy.IGNORE)
+   long[] insert(Incident... incidents);
+
+   @Query("DELETE FROM incident")
+   void deleteAllIncidents();
+
+   @Query("DELETE FROM report")
+   void deleteAllReports();
+
+   /*@Transaction
+   @Query("SELECT * from report ORDER BY report_id DESC LIMIT 10")
+   LiveData<List<ReportWithIncidents>> getLastReportsWithIncidents();*/
+
+   @Transaction
+   @Query("SELECT * from report ORDER BY report_id DESC LIMIT 10")
+   LiveData<List<ReportWithIncidents>> getLastReportsWithIncidents();
+
+   @Transaction
+   @Query("SELECT * FROM report")
+   LiveData<List<ReportWithIncidents>> getReportsWithIncidents();
+
+}
